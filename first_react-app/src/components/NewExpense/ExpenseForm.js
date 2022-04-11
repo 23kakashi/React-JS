bba6b59d5edfd83b41f3ce13ea494f1d5ba34b16
Tapish sharma <tapishsharma1234@gmail.com>
2022-04-11 21:57:52 +0530

    Expense manager completed
    Props, state, upstaging , two way binding, list and conditional content

--- first_react-app/src/components/NewExpense/ExpenseForm.js
+++ first_react-app/src/components/NewExpense/ExpenseForm.js
@@ -44,11 +44,13 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
     props.onSaveExpenseData(expenseData);
+
+    //Two way binding
     setEnteredTitle('');
     setEnteredAmount('');
     setEnteredDate('');
@@ -87,6 +89,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button type='button' onClick={props.onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
